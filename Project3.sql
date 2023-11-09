b8e47f933252801ce54b109f610ab88b536a7a7b
Landonie314 <trumpet.landon@gmail.com>
2023-11-09 14:19:44 -0600

    FKs and Drop Table
    Added most to if all foreign keys and added drop table to the beginning of the code.

--- Project3.sql
+++ Project3.sql
@@ -1,3 +1,23 @@
+DROP TABLE Writer CASCADE CONSTRAINTS;
+DROP TABLE Actor CASCADE CONSTRAINTS;
+DROP TABLE Director CASCADE CONSTRAINTS;
+DROP TABLE Movie CASCADE CONSTRAINTS;
+DROP TABLE TV_show CASCADE CONSTRAINTS;
+DROP TABLE TV_write CASCADE CONSTRAINTS;
+DROP TABLE TV_act CASCADE CONSTRAINTS;
+DROP TABLE TV_direct CASCADE CONSTRAINTS;
+DROP TABLE MV_write CASCADE CONSTRAINTS;
+DROP TABLE MV_act CASCADE CONSTRAINTS;
+DROP TABLE MV_direct CASCADE CONSTRAINTS;
+DROP TABLE MV_awards CASCADE CONSTRAINTS;
+DROP TABLE TV_show_awards CASCADE CONSTRAINTS;
+DROP TABLE MV_genres CASCADE CONSTRAINTS;
+DROP TABLE Production_company CASCADE CONSTRAINTS;
+DROP TABLE MV_reviews CASCADE CONSTRAINTS;
+DROP TABLE TV_reviews CASCADE CONSTRAINTS;
+
+
+
 CREATE TABLE Writer
   (Height	NUMBER(3,2),
    Birthday	DATE,
@@ -108,29 +128,98 @@ CREATE TABLE Production_company
    CONSTRAINT	production_idcompany_pk PRIMARY KEY (Id, Company)
 );
 
-CREATE TABLE TV_Reviews
+CREATE TABLE TV_reviews
   (Id		CHAR(9),
    Username	VARCHAR2(25),
    Title	VARCHAR2(25),
-   Date		DATE,
-   Critics	VARCHAR(1),
+   Review_date		DATE,
+   Critics	NUMBER(3),
    Account_date	DATE,
    CONSTRAINT tv_reviews_iduser_pk PRIMARY KEY (Id, Username)
 );
 
-CREATE TABLE MV_Reviews
+CREATE TABLE MV_reviews
   (Id		CHAR(9),
    Username	VARCHAR2(25),
    Title	VARCHAR2(25),
-   Date		DATE,
-   Critics	VARCHAR2(1),
+   Review_date		DATE,
+   Critics	NUMBER(3),
    Metacritic	VARCHAR2(1),
    Account_date	DATE,
    CONSTRAINT mv_reviews_iduser_pk PRIMARY KEY (Id, Username)
 );
 
 
+ALTER TABLE TV_write
+ADD CONSTRAINT tv_write_name_fk FOREIGN KEY (Name)
+REFERENCES Writer (Name);
 
+ALTER TABLE TV_write
+ADD CONSTRAINT tv_write_id_fk FOREIGN KEY (Id)
+REFERENCES Tv_show (Id);
 
+ALTER TABLE TV_act
+ADD CONSTRAINT tv_act_name_fk FOREIGN KEY (Name)
+REFERENCES Actor (Name);
 
-     
+ALTER TABLE TV_act
+ADD CONSTRAINT tv_act_id_fk FOREIGN KEY (Id)
+REFERENCES Tv_show (Id);
+
+ALTER TABLE TV_direct
+ADD CONSTRAINT tv_direct_name_fk FOREIGN KEY (Name)
+REFERENCES Director (Name);
+
+ALTER TABLE TV_direct
+ADD CONSTRAINT tv_direct_id_fk FOREIGN KEY (Id)
+REFERENCES Tv_show (Id);
+
+ALTER TABLE MV_write
+ADD CONSTRAINT mv_write_name_fk FOREIGN KEY (Name)
+REFERENCES Writer (Name);
+
+ALTER TABLE MV_write
+ADD CONSTRAINT mv_write_id_fk FOREIGN KEY (Id)
+REFERENCES Movie (Id);
+
+ALTER TABLE TV_act
+ADD CONSTRAINT mv_act_name_fk FOREIGN KEY (Name)
+REFERENCES Actor (Name);
+
+ALTER TABLE TV_act
+ADD CONSTRAINT mv_act_id_fk FOREIGN KEY (Id)
+REFERENCES Movie (Id);
+
+ALTER TABLE TV_direct
+ADD CONSTRAINT mv_direct_name_fk FOREIGN KEY (Name)
+REFERENCES Director (Name);
+
+ALTER TABLE TV_direct
+ADD CONSTRAINT mv_direct_id_fk FOREIGN KEY (Id)
+REFERENCES Movie (Id);
+
+ALTER TABLE Movie_awards
+ADD CONSTRAINT mv_awards_id_fk FOREIGN KEY (Id)
+REFERENCES Movie (Id);
+
+ALTER TABLE TV_show_awards
+ADD CONSTRAINT tv_awards_id_fk FOREIGN KEY (Id)
+REFERENCES TV_show (Id);
+
+ALTER TABLE Mv_genres
+ADD CONSTRAINT mv_genres_id_fk FOREIGN KEY (Id)
+REFERENCES Movie (Id);
+
+ALTER TABLE Production_company
+ADD CONSTRAINT company_id_fk FOREIGN KEY (Id)
+REFERENCES Movie (Id);
+
+ALTER TABLE TV_reviews
+ADD CONSTRAINT tv_reviews_id_fk FOREIGN KEY (Id)
+REFERNCES TV_show (Id);
+
+ALTER TABLE MV_reviews
+ADD CONSTRAINT mv_reviews_id_fk FOREIGN KEY (Id)
+REFERNCES Movie (Id);
+
+    
